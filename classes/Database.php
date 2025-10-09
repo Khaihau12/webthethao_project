f90f94dbb1aa5205e88c1616c3e3f12dae3746a1
Khaihau12 <khaihau100@gmail.com>
2025-10-09 12:31:39 +0700

    Create Database.php

---
+++ classes/Database.php
@@ -0,0 +1,30 @@
+<?php
+// Tên file: classes/Database.php
+class Database {
+    private static $instance = null;
+    private $conn;
+
+    private function __construct() {
+        $this->conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        if ($this->conn->connect_error) {
+            die("Kết nối database thất bại: " . $this->conn->connect_error);
+        }
+        $this->conn->set_charset("utf8mb4");
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    // Ngăn chặn việc clone đối tượng
+    private function __clone() {}
+}
+?>
